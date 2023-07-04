2e284c2568943a29c704e180b8024aeb99797753
rizkynandaanggia <127828416+rizkynandaanggia@users.noreply.github.com>
2023-07-04 07:04:47 +0700

    Add files via upload

---
+++ Praktikum Minggu 12/Praktikum Minggu 12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PraktikumMinggu12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrNim = { 2021, 2022, 2023 };
+            string[] arrNama = { "Anggi", "Adiba", "Kevin" };
+            int[] arrNilai = { 80, 87, 93 };
+
+            Console.WriteLine("No.          Nim       Nama       Nilai");
+            Console.WriteLine("=======================================");
+
+            for (int i = 0; i < arrNama.Length; i++)
+            {
+                Console.WriteLine("{0}.           {1}      {2}       {3}", i + 1, arrNim[i], arrNama[i], arrNilai[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
